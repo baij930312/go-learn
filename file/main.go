3f6dd6859306e6dcf8d7ba6edc9e447d35306e9a
jinWarr <baij930312@163.com>
2019-07-14 23:47:35 +0800

    bufio writer

--- file/main.go
+++ file/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"bufio"
 )
 
 func main (){
-	fmt.Println("hellp world")
+	file ,err := os.OpenFile("./temp.txt", os.O_WRONLY | os.O_CREATE,0666)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	wr := bufio.NewWriter(file);
+	wr.WriteString("123123")
+	//刷新缓冲区
+	wr.Flush()
 }
