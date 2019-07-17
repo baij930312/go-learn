cd4378ce108d17293471ae9c1259a3666de02aba
jinWarr <baij930312@163.com>
2019-07-17 22:25:42 +0800

    groutine demo

--- file/main.go
+++ file/main.go
@@ -1,42 +1,23 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
+	"fmt"
+	"time"
 )
 
-type Monster struct {
-	Name  string `json:"name"`
-	Age   int    `json:"age"`
-	Skill string `json:"skill"`
+func test() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("test", i)
+		time.Sleep(time.Second)
+
+	}
 }
 
-func (this *Monster) Store() bool {
-	content, err := json.Marshal(this)
-	if err != nil {
-		return false
-	}
-	err = ioutil.WriteFile("./Monster.txt", content, 0666)
-	if err != nil {
-		return false
-	}
-	return true
-}
+func main() {
+	go test()
+	for i := 0; i < 10; i++ {
+		fmt.Println("main", i)
+		time.Sleep(time.Second)
 
-// "./Monster.txt"
-func (this *Monster) ReStore(path string) bool {
-	content,err := ioutil.ReadFile(path)
-	if err != nil {
-		return false
 	}
-	err = json.Unmarshal(content,this)
-	if err != nil {
-		return false
-	}
-	return true
-}
-
-
-func main()  {
-	
 }
