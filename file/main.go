07fea6b9b3921a8fe9dd47d9d693e4c256d3c338
jinWarr <baij930312@163.com>
2019-07-18 23:23:32 +0800

    chan

--- file/main.go
+++ file/main.go
@@ -2,32 +2,12 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
-
-var (
-	m    = make(map[int]int)
-	lock sync.Mutex
-)
-
-func test(n int) {
-	res := 1
-	for index := 1; index <= n; index++ {
-		res *= index
-	}
-	lock.Lock()
-	m[n] = res
-	lock.Unlock()
-}
 
 func main() {
-	for index := 0; index < 100; index++ {
-		go test(index)
-	}
+	channel := make(chan int, 3)
+	channel <- 2
 
-	time.Sleep(time.Second * 3)
-	lock.Lock()
-	fmt.Println(m )
-	lock.Unlock()
+	fmt.Println(<-channel)
+	fmt.Println(len(channel), "  ", cap(channel))
 }
