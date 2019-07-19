ca5b4ab73a31c89ce5e5487d5795606aff434b3d
jinWarr <baij930312@163.com>
2019-07-19 14:29:55 +0800

    recover  panic

--- file/main.go
+++ file/main.go
@@ -5,29 +5,21 @@ import (
 	"time"
 )
 
-func writeData(ch chan int) {
-	for i := 0; i < 10; i++ {
-		time.Sleep(time.Second * 2)
-		ch <- i
-	}
-}
-func main() {
-	ch1 := make(chan int, 10)
-	ch2 := make(chan int, 10)
+func test() {
 
-	go writeData(ch1)
-	go writeData(ch2)
-
-	for {
-		select {
-		case v := <-ch1:
-			fmt.Println(v)
-		case v := <-ch2:
-			fmt.Println(v)
-		default:
-			time.Sleep(time.Second / 2)
-			fmt.Println("no data ")
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
 		}
-	}
+	}()
+	panic("asdsa")
+	// var m map[string]int
+	// m["123"] = 1
+}
+
+func main() {
+	go test()
+
+	time.Sleep(time.Second)
 
 }
