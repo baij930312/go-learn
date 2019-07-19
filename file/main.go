20578a8ebf8c00cd7b0466286bdf459887af5c14
jinWarr <baij930312@163.com>
2019-07-19 14:21:43 +0800

    select chan

--- file/main.go
+++ file/main.go
@@ -1,55 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func isPrimeNumber(n int) bool {
-	for i := 2; i < n; i++ {
-		if (n % i) == 0 {
-			return false
-		}
+func writeData(ch chan int) {
+	for i := 0; i < 10; i++ {
+		time.Sleep(time.Second * 2)
+		ch <- i
 	}
-	return true
 }
-
-func run(source chan int, result chan int, exit chan bool) {
-	for {
-		n, err := <-source
-		if !err {
-			break
-		}
-		if isPrimeNumber(n) {
-			result <- n
-		}
-	}
-	exit <- true
-}
-
 func main() {
-	source := make(chan int, 8000)
-	result := make(chan int, 8000)
-	exit := make(chan bool, 4)
-	go func() {
-		for i := 0; i < 8000; i++ {
-			source <- i
-		}
-		close(source)
-	}()
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
 
-	for i := 0; i < 4; i++ {
-		go run(source, result, exit)
+	go writeData(ch1)
+	go writeData(ch2)
+
+	for {
+		select {
+		case v := <-ch1:
+			fmt.Println(v)
+		case v := <-ch2:
+			fmt.Println(v)
+		default:
+			time.Sleep(time.Second / 2)
+			fmt.Println("no data ")
+		}
 	}
 
-	go func() {
-		for i := 0; i < 4; i++ {
-			<-exit
-		}
-		close(result) //为啥不写这一行会报错````` //因为主携程在等待result明确推出 否则会造成主携程思索
-	}()
-
-	for n := range result {
-		
-		fmt.Println(n)
-	}
-	close(exit)
-	fmt.Println("over")
 }
