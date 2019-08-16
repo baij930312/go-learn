4a19c84482ad40c2f334b80904f1163f348a7fe2
jinWarr <baij930312@163.com>
2019-08-16 14:41:58 +0800

    return a new slice for bubble soft

--- file/data_struct/bubblesort/main.go
+++ file/data_struct/bubblesort/main.go
@@ -7,11 +7,13 @@ import (
 func main() {
 	arr := []int{23, 2, 1221, 453, 43, 213, 12, 432, 78, 1}
 	fmt.Println(arr)
-	bubbleSoft(arr)
+	arr = bubbleSoft(arr)
 	fmt.Println(arr)
 }
 
-func bubbleSoft(arr []int) {
+func bubbleSoft(rowarr []int) []int {
+	arr := make([]int, 10)
+	copy(arr, rowarr)
 	for i := 0; i < len(arr); i++ {
 		for j := i + 1; j < len(arr); j++ {
 			if arr[i] > arr[j] {
@@ -19,4 +21,5 @@ func bubbleSoft(arr []int) {
 			}
 		}
 	}
+	return arr
 }
